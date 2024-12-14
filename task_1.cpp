b29f2789e1a111ec220ac48e35c501a7630dc10a
arham_shahid <arhamshahid005@gmail.com>
2024-12-14 10:15:07 +0500

    endl role

--- task_1.cpp
+++ task_1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 // Run the following code and see the output
 int main()
 {
-    cout << "\nHello Batch23" << endl;
+    cout << "\nHello Batch24" << endl;
     cout << "Hi there" << endl
          << endl;
     cout << "Hello here ";
