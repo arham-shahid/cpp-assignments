cc66c2aaba59acd4bc24465ef4681cd3f7d1f6d6
arham_shahid <arhamshahid005@gmail.com>
2024-12-14 10:13:01 +0500

    endl role

---
+++ task_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// Run the following code and see the output
+int main()
+{
+    cout << "\nHello Batch23" << endl;
+    cout << "Hi there" << endl
+         << endl;
+    cout << "Hello here ";
+    return 0;
+}
